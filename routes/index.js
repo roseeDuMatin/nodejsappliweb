53fbe1e9af131550bcf62addcd753cfb41e69dad
roseeDuMatin <s_pelluet@hotmail.fr>
2020-03-19 15:57:36 +0100

    formulaire ville + route get

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
 
+router.get('/ville', function(req, res, next){
+    res.render('ville', { ville: req.query.ville });
+})
+
 module.exports = router;
