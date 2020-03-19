aa844296fbf9ed2f244e146aeace555a48dcb25a
roseeDuMatin <s_pelluet@hotmail.fr>
2020-03-19 16:03:10 +0100

    formulaire de ville passé en POST

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
 
-router.get('/ville', function(req, res, next){
-    res.render('ville', { ville: req.query.ville });
+router.post('/ville', function(req, res, next){
+    res.render('ville', { ville: req.body.ville });
 })
 
 module.exports = router;
